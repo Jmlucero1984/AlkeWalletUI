b83d751ee1ee5a8ad6c9ffad8b7823ff92fb6ebe
jmlucero1984 <jmlucero1984@gmail.com>
2026-02-14 23:28:50 -0300

    Initial commit

---
+++ app/src/main/java/com/jmlucero/alkewallet/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.jmlucero.alkewallet
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.time.delay
+
+class MainActivity : AppCompatActivity() {
+    private var keepSplashOnScreen = true
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        splashScreen.setKeepOnScreenCondition {
+            keepSplashOnScreen
+        }
+        lifecycleScope.launch {
+            delay(4000) // 2 segundos
+            keepSplashOnScreen = false
+        }
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
