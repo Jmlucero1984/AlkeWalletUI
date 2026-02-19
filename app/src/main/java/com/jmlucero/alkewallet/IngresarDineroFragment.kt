2b68fef52a608442ce1ff4e1974a16ce3666d7e9
jmlucero1984 <jmlucero1984@gmail.com>
2026-02-18 23:19:30 -0300

    Add bell icon toggle visible/invisible transactions

--- app/src/main/java/com/jmlucero/alkewallet/IngresarDineroFragment.kt
+++ app/src/main/java/com/jmlucero/alkewallet/IngresarDineroFragment.kt
@@ -5,13 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
+import com.jmlucero.alkewallet.databinding.FragmentEnviarDineroBinding
 import com.jmlucero.alkewallet.databinding.FragmentHomePageBinding
-
+import com.jmlucero.alkewallet.databinding.FragmentIngresarDineroBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,14 +17,14 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomePageFragment.newInstance] factory method to
+ * Use the [IngresarDineroFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomePageFragment : Fragment() {
+class IngresarDineroFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private var _binding: FragmentHomePageBinding? = null
+    private var _binding: FragmentIngresarDineroBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,44 +33,24 @@ class HomePageFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-
-
-        //findNavController().navigate(R.id.homeActivity)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        _binding = FragmentHomePageBinding.inflate(inflater, container, false)
+        _binding = FragmentIngresarDineroBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.usuarioProfileImg.setOnClickListener {
-            findNavController().navigate(R.id.profileActivity)
+        binding.backButton.setOnClickListener {
+            findNavController().navigate(R.id.homePageFragment)
         }
-        binding.enviarDineroButton.setOnClickListener {
-            findNavController().navigate(R.id.action_home_to_enviarDinero)
-        }
-        binding.ingresarDineroButton.setOnClickListener {
-            findNavController().navigate(R.id.action_home_to_ingresarDinero)
-        }
-
-        binding.campanitaIcon.setOnClickListener {
-            binding.frameLayoutEmptyTransactions.isVisible =
-                !binding.frameLayoutEmptyTransactions.isVisible
-            binding.fragmentContainerView.isVisible =
-                !binding.fragmentContainerView.isVisible
-        }
-
-
 
     }
-
 
     companion object {
         /**
@@ -82,12 +59,12 @@ class HomePageFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomePageFragment.
+         * @return A new instance of fragment IngresarDineroFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomePageFragment().apply {
+            IngresarDineroFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
